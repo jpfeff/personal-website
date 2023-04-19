70b535366f48b09eced3d8754b27c141eafc026d
Joshua Pfefferkorn <77121865+jpfeff@users.noreply.github.com>
2023-04-19 13:26:47 -0400

    added routing

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,53 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import {
+  BrowserRouter, Routes, Route, NavLink, useParams,
+} from 'react-router-dom';
 import './style.scss';
 
+function About(props) {
+  return <div> All there is to know about me </div>;
+}
+function Welcome(props) {
+  return <div>Welcome</div>;
+}
+
+function Test(props) {
+  const { id } = useParams();
+  return <div> ID: {id} </div>;
+}
+
+function FallBack(props) {
+  return <div>URL Not Found</div>;
+}
+
+function Nav(props) {
+  return (
+    <nav>
+      <ul>
+        <li><NavLink to="/">Home</NavLink></li>
+        <li><NavLink to="/about">About</NavLink></li>
+        <li><NavLink to="/test/id1">test id1</NavLink></li>
+        <li><NavLink to="/test/id2">test id2</NavLink></li>
+      </ul>
+    </nav>
+  );
+}
+
 function App() {
-  return <div className="test">All the REACT are belong to us!</div>;
+  return (
+    <BrowserRouter>
+      <div>
+        <Nav />
+        <Routes>
+          <Route path="/" element={<Welcome />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/test/:id" element={<Test />} />
+          <Route path="*" element={<FallBack />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 const root = createRoot(document.getElementById('main'));
